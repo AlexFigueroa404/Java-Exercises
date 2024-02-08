557d40ee60345409bdef1b312bc271d4f47cdb77
Alex Figueroa <alex.figue503@gmail.com>
2024-02-08 11:41:06 -0600

    0001

---
+++ src/Exercise_0001.java
@@ -0,0 +1,13 @@
+/*Imprimir los elementos de un arreglo en orden ascendente*/
+
+public class Exercise_0001 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+",");
+        }
+
+    }
+}
